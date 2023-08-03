f41e27f9eb73df1d290edbd03458d44cb682a8c3
lakshmineha577 <81248720+lakshmineha577@users.noreply.github.com>
2023-08-03 12:18:57 +0530

    Add files via upload

---
+++ ProjectFiles/Model.kt
@@ -0,0 +1,6 @@
+package com.example.newsheadlines
+
+data class Model(val name: String,
+                 val imageUrl: String,
+                 val desc: String,
+                 val category: String)
